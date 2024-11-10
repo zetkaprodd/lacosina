b5de0215843dc686bddafa82fd6cacb4b1aa82db
Ebayart <elibayart05@gmail.com>
2024-11-10 14:47:26 +0100

    fix: little mistake on detail

--- lacosina/src/Views/Recettes/detail.php
+++ lacosina/src/Views/Recettes/detail.php
@@ -1,7 +1,7 @@
 <h1>
     <?php echo 'Titre de la recette :' ?>
     <?php echo $recipe['titre'] ?>
-    <img src="upload/<?php echo ((isset($recipe['image']) && $recipe['image'] != "") ? $recipe['image'] : "no_image.png"); ?>" alt="../upload/no">
+    <img src="upload/<?php echo ((isset($recipe['image']) && $recipe['image'] != "") ? $recipe['image'] : "no_image.png"); ?>" alt="../upload/no_image.png">
     </h1><br>
     <?php echo 'Description :' ?>
     <?php echo $recipe['description'] ?>
